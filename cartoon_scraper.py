b1cbdda70b6cb59db653ec18b7c57d064ee7759b
zfudge <zacheryfudge@gmail.com>
2018-12-05 08:22:16 -0700

    Converted from JS output to mp4 reading/writing
     Changes to be committed:
    	modified:   cartoon_scraper.py

--- cartoon_scraper.py
+++ cartoon_scraper.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import datetime
 import requests
 import logging
+import urllib2
 import base64
 import random
 import json
 import time
 import sys
 import re
+import os
 
 import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
@@ -47,12 +49,14 @@ def landing_loop(scripts):
 			return get_vars(str(script))
 
 def soup_request(url):
+	logging.info("{}\n".format(datetime.datetime.now()))
+	logging.info(url)
 	r = requests.get(url)
 	return BeautifulSoup(r.content, 'html.parser')
 
 def get_show_urls(show_name, start_season, seasons, start_episode, episodes):
 	logging.info("{}: seasons {}-{}, episodes {}-{}\n".format(show_name, start_season, start_season + seasons - 1, start_episode, start_episode + episodes - 1))
-	url = 'https://www.watchcartoononline.io/{}-season-{}-episode-{}'
+	url_1 = 'https://www.watchcartoononline.io/{}-season-{}-episode-{}'
 	show = {
 		"show_name": show_name,
 		"urls" : []
@@ -60,13 +64,18 @@ def get_show_urls(show_name, start_season, seasons, start_episode, episodes):
 	for season in range(start_season, start_season + seasons):
 		for episode in range(start_episode, start_episode + episodes):
 			logging.info("Season {} Episode {}".format(season, episode))
-			logging.info("{}\n".format(datetime.datetime.now()))
-			first_soup = soup_request(url.format(show_name, season, episode))
+			first_soup = soup_request(url_1.format(show_name, season, episode))
 			landing_scripts = first_soup.select("script")
 			iframe = landing_loop(landing_scripts)
  			# Call iframe
 			try:
 				url_2 = "https://www.watchcartoononline.io{}".format(iframe[3])
+				try:
+					name = url_2[url_2.index("{}.".format(episode)) + 2 : -1 * (len(url_2[url_2.index("{}.".format(episode))+2:]) - re.search("[0-9]", url_2[url_2.index("{}.".format(episode))+2:]).start())]
+				except:
+					name = re.sub('[0-9]', '', url_2[url_2.index("=") + 1 : -1 * (len(url_2[url_2.index("=") + 1:]) - url_2[url_2.index("=") + 1:].index("&"))]).replace("%", '')
+				except:
+					name = ''
 			except:
 				logging.error("Error on Season {} Episode {}".format(season, episode))
 				logging.error(iframe)
@@ -78,45 +87,40 @@ def get_show_urls(show_name, start_season, seasons, start_episode, episodes):
 			show["urls"].append({
 				"season": season,
 				"episode": episode,
-				"url":	url_3.replace("lbb.", "media4.")
+				"url":	url_3.replace("lbb.", "media4."),
+				"name": name
 			})
-			time.sleep(round(random.uniform(7, 16), 3))
+			time.sleep(round(random.uniform(7, 16), 6))
 	return show
-
-def list_urls(show_dict):
-	content = ""
-	show_name = show_dict["show_name"].split("-")
-	for i, n in enumerate(show_name): show_name[i] = "{}{}".format(n[:1].upper(), n[1:])
-	show_name = " ".join(show_name)
-	for show in show_dict["urls"]:
-		content += "{} - Season {} Episode {}\n".format(show_name, show["season"], show["episode"])
-		content += show["url"] + "\n\n"
-	write_output(content, "a")
-
-def format_urls(show_dict):
-	js = "(function o(arr, index = 0) {\n"
-	js += 'window.open(arr[index], "_blank")\n'
-	js += "setTimeout(() => (index < arr.length - 1) ? o(arr, index + 1) : null, Math.round(Math.random() * 7000 + 13000))"
-	js += "}([\n"
-	for url in show_dict["urls"]:
-		js += '\t"{}",\n'.format(url["url"])
-	js = js[:-2] + "\n"
-	js += "]))\n\n"
-	write_output(js, "w")
-	show_dict["javascript"] = js
-	return show_dict
-
-def write_output(content, mode):
-	file = open("logs.txt", mode)
-	file.write(content)
-	file.close()
 
 def log_data(data):
 	with open("cartoon_logs.json", "a") as f:
 	    json.dump(data, f)
 
+def write_mp4s(show_dict):
+	if not os.path.exists(show_dict['show_name']):
+		logging.info("Creating {} directory".format(show_dict['show_name']))
+		os.makedirs(show_dict['show_name'])
+	os.chdir(show_dict['show_name'])
+	logging.info("Changing to {} directory".format(show_dict['show_name']))
+	for url in show_dict['urls']:
+		file_name = '{}.{}-{}-{}.mp4'.format(url['season'], url['episode'], show_dict['show_name'], url["name"])
+		write_mp4(url, file_name)
+
+def write_mp4(url, file_name):
+	try:
+		logging.info("\n\n{}\n".format(datetime.datetime.now()))
+		logging.info(url["url"])
+		response = urllib2.urlopen(url["url"])
+		with open(file_name, 'wb') as f:
+			logging.info("Writing {}".format(file_name))
+			f.write(response.read())
+			f.close()
+			time.sleep(round(random.uniform(7, 14), 6))
+	except:
+		pass
+
 if __name__ == '__main__':
-	urls = get_show_urls("", 2, 1, 1, 2)
-	urls = format_urls(urls)
-	list_urls(urls)
+	urls = get_show_urls("", 2, 1, 1, 1)
 	log_data(urls)
+	write_mp4s(urls)

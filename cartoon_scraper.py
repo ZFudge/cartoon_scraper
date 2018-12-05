6374bfabcf82085d36c9be8c6bbad9ca7dd25df6
zfudge <zacheryfudge@gmail.com>
2018-12-05 13:29:10 -0700

    Consecutive requests per episode, remove sleep, pretty printed logs
     Changes to be committed:
    	modified:   cartoon_scraper.py

--- cartoon_scraper.py
+++ cartoon_scraper.py
@@ -6,13 +6,24 @@ import urllib2
 import base64
 import random
 import json
-import time
 import sys
 import re
 import os
 
 import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+def soup_request(url):
+	logging.info("Requesting {}\n".format(url))
+	r = requests.get(url)
+	return BeautifulSoup(r.content, 'html.parser')
+
+def show_directory(show):
+	if not os.path.exists(show):
+		logging.info("Creating {} directory".format(show))
+		os.makedirs(show)
+	logging.info("Changing to {} directory\n".format(show))
+	os.chdir(show)
 
 def get_vars(script):
 	script = script[33:]
@@ -48,14 +59,9 @@ def landing_loop(scripts):
 		if len(str(script).split('", "')) > 170:
 			return get_vars(str(script))
 
-def soup_request(url):
-	logging.info("{}\n".format(datetime.datetime.now()))
-	logging.info(url)
-	r = requests.get(url)
-	return BeautifulSoup(r.content, 'html.parser')
-
-def get_show_urls(show_name, start_season, seasons, start_episode, episodes):
+def get_episodes(show_name, start_season, seasons, start_episode, episodes):
 	logging.info("{}: seasons {}-{}, episodes {}-{}\n".format(show_name, start_season, start_season + seasons - 1, start_episode, start_episode + episodes - 1))
+	show_directory(show_name)
 	url_1 = 'https://www.watchcartoononline.io/{}-season-{}-episode-{}'
 	show = {
 		"show_name": show_name,
@@ -63,7 +69,7 @@ def get_show_urls(show_name, start_season, seasons, start_episode, episodes):
 	}
 	for season in range(start_season, start_season + seasons):
 		for episode in range(start_episode, start_episode + episodes):
-			logging.info("Season {} Episode {}".format(season, episode))
+			logging.info("Season {} Episode {}\n".format(season, episode))
 			first_soup = soup_request(url_1.format(show_name, season, episode))
 			landing_scripts = first_soup.select("script")
 			iframe = landing_loop(landing_scripts)
@@ -72,55 +78,51 @@ def get_show_urls(show_name, start_season, seasons, start_episode, episodes):
 				url_2 = "https://www.watchcartoononline.io{}".format(iframe[3])
 				try:
 					name = url_2[url_2.index("{}.".format(episode)) + 2 : -1 * (len(url_2[url_2.index("{}.".format(episode))+2:]) - re.search("[0-9]", url_2[url_2.index("{}.".format(episode))+2:]).start())]
-				except:
+				except Exception as e:
 					name = re.sub('[0-9]', '', url_2[url_2.index("=") + 1 : -1 * (len(url_2[url_2.index("=") + 1:]) - url_2[url_2.index("=") + 1:].index("&"))]).replace("%", '')
 				except:
 					name = ''
 			except:
 				logging.error("Error on Season {} Episode {}".format(season, episode))
-				logging.error(iframe)
-				sys.exit()
+				logging.error("{}\n\n".format(iframe))
+				continue
 			second_soup = soup_request(url_2)
 			video_scripts = second_soup.select("script")
 			# Extract hosting endpoint 
 			url_3 = iframe_looper(video_scripts)
-			show["urls"].append({
+			file_name = '{}.{}-{}_{}.mp4'.format(season, episode, show_name, name)
+			episode_dict = {
 				"season": season,
 				"episode": episode,
 				"url":	url_3.replace("lbb.", "media4."),
 				"name": name
-			})
-			time.sleep(round(random.uniform(7, 16), 6))
+			}
+			show["urls"].append(episode_dict)
+			write_mp4(episode_dict["url"], file_name)
 	return show
-
-def log_data(data):
-	with open("cartoon_logs.json", "a") as f:
-	    json.dump(data, f)
-
-def write_mp4s(show_dict):
-	if not os.path.exists(show_dict['show_name']):
-		logging.info("Creating {} directory".format(show_dict['show_name']))
-		os.makedirs(show_dict['show_name'])
-	os.chdir(show_dict['show_name'])
-	logging.info("Changing to {} directory".format(show_dict['show_name']))
-	for url in show_dict['urls']:
-		file_name = '{}.{}-{}-{}.mp4'.format(url['season'], url['episode'], show_dict['show_name'], url["name"])
-		write_mp4(url, file_name)
 
 def write_mp4(url, file_name):
 	try:
-		logging.info("\n\n{}\n".format(datetime.datetime.now()))
-		logging.info(url["url"])
-		response = urllib2.urlopen(url["url"])
+		logging.info("Retrieving mp4 from {}\n".format(url))
+		response = urllib2.urlopen(url)
 		with open(file_name, 'wb') as f:
 			logging.info("Writing {}".format(file_name))
 			f.write(response.read())
-			f.close()
-			time.sleep(round(random.uniform(7, 14), 6))
+			logging.info("Writing complete\n\n")
 	except:
+		logging.error("Error writing {}\n\n".format(file_name))
 		pass
 
+def log_data(data):
+	log_name = "cartoon_logs.json"
+	logging.info("Changing to parent directory")
+	os.chdir("..")
+	with open(log_name, "a") as f:
+		logging.info("Dumping json data to {}".format(log_name))
+		f.write(",\n")
+		json.dump(data, f, indent=4, sort_keys=True)
+	logging.info("Goodbye")
+
 if __name__ == '__main__':
-	urls = get_show_urls("", 2, 1, 1, 1)
+	urls = get_episodes(shows[3], 1, 1, 1, 1)
 	log_data(urls)
-	write_mp4s(urls)
